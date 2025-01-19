d6b2a9b8ddc5a2f81ddbc1a8d9207fca2d36ad6a
Yauri Attamimi <yauritux@gmail.com>
2025-01-19 13:42:51 +0700

    fix: set wso2mi default uuid if not provided

--- schema.sql
+++ schema.sql
@@ -3,5 +3,5 @@ create extension if not exists "uuid-ossp";
 CREATE TABLE camel_write_db(id uuid not null primary key, data json, time_ms bigint, created_at timestamp default now());
 CREATE TABLE camel_read_db(id uuid not null primary key, data json, time_ms bigint, created_at timestamp default now());
 
-CREATE TABLE wso2mi_write_db(id uuid not null primary key, data json, time_ms bigint, created_at timestamp default now());
-CREATE TABLE wso2mi_read_db(id uuid not null primary key, data json, time_ms bigint, created_at timestamp default now());
+CREATE TABLE wso2mi_write_db(id uuid not null primary key default uuid_generate_v4(), data json, time_ms bigint, created_at timestamp default now());
+CREATE TABLE wso2mi_read_db(id uuid not null primary key default uuid_generate_v4(), data json, time_ms bigint, created_at timestamp default now());
